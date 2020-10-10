8c8678a4181ff334abe6973bedc4409d35bf1f84
Esraa-Yousef-Alhabeeb-042 <66417675+Esraa-Yousef-Alhabeeb-042@users.noreply.github.com>
2020-10-10 20:39:01 +0300

    done

--- scripts.js
+++ scripts.js
@@ -1,13 +1,33 @@
 // 1
 // moon.jpg قم بالوصول إلى الصورة وتغيرها إلى
-
+let image = document.getElementById("image")
+image.src = "./images/moon.jpg"
 // 2
 // "قم بالوصول إلى كلمة الأرض وتحويلها إلى "القمر
-
+let title = document.getElementById("caption")
+title.innerHTML = "القمر";
 // 3
 // بحيث إن ضغطت على البطاقة تتغير الصورة للصورة الأخرى ويتغير الاسم onClick قم بإنشاء دالة واستخدام خاصية
 // دون الحاجة إلى الخطوتين 2و1
+function change() {
+    let title = document.getElementById("caption");
+    let image = document.getElementById("image");
+    
 
+    if (title.innerHTML == "القمر") {
+       
+        title.innerHTML = "الشمس";
+        image.src = "./images/sun.jpg"
+       document.body.style.backgroundColor = `#ff9f1c`
+    }
+    else {
+        title.innerHTML = "القمر";
+        image.src = "./images/moon.jpg"
+       document.body.style.backgroundColor = `#212529`
+    }
+
+
+}
 // 4
 // Night - Light Mode قم بالتعديل على الدالة بحيث تضم إليها خاصية
 
